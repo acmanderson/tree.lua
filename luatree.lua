51acf2863653ac2c35c880681591886b6b5bb479
Chase <acmanderson@gmail.com>
2014-08-04 23:47:08 -0400

    Fixed issue with using '.' as a filepath. Some refactoring to improve clarity. Preliminary implementation of command line arguments, currently only -a for showing hidden files.

--- luatree.lua
+++ luatree.lua
@@ -1,7 +1,35 @@
+#!/usr/bin/env lua
+
 require "lfs"
 
 local SEPARATOR = '/'
-local path = arg[1] or '.'
+local DEFAULT_INDENT = '   '
+local VERTICAL_BAR = '┃'
+local MIDDLE_CONNECTOR = "┣"
+local ENDING_CONNECTOR = "┗"
+local HORIZONTAL_CONNECTOR = "━━ "
+
+local TREE_ARGS = {a=false}
+
+local num_dirs = 0
+local num_files = 0
+
+local function get_arg_from_string(arg_string)
+	return (arg_string:gsub('^%-*', ''))
+end
+
+local path
+for i = 1, #arg do
+	if not arg[i]:match('^%-') then
+		path = arg[i]
+	else
+		local cli_arg = get_arg_from_string(arg[i])
+		if TREE_ARGS[cli_arg] ~= nil then
+			TREE_ARGS[cli_arg] = true
+		end
+	end
+end
+path = path or '.'
 
 function string:split(sep)
     local sep, fields = sep or ":", {}
@@ -26,13 +54,18 @@ local function get_dirs_and_files(dir)
     local function yieldtree(dir)
         for entry in lfs.dir(dir) do
             if entry ~= "." and entry ~= ".." then
-                entry=dir.."/"..entry
-                local attr=lfs.attributes(entry)
-                coroutine.yield(entry,attr)
-                if attr.mode == "directory" then
-                    yieldtree(entry)
-                end
-            end
+            	if not entry:match('^%.') or TREE_ARGS['a'] then
+	                entry=dir.."/"..entry
+	                local attr=lfs.attributes(entry)
+	                coroutine.yield(entry,attr)
+	                if attr.mode == "directory" then
+	                	num_dirs = num_dirs + 1
+	                    yieldtree(entry)
+	                else
+	                	num_files = num_files + 1
+	                end
+	            end
+	        end
         end
     end
 
@@ -43,7 +76,7 @@ local function get_dir_tree()
     local master = {[path]={}}
     local current = master[path]
     for filename, attr in get_dirs_and_files(path) do
-        for i, part in pairs((filename:gsub(path, '')):split(SEPARATOR)) do
+        for i, part in pairs((filename:gsub(path, '', 1)):split(SEPARATOR)) do
             if not current[part] then
                 current[part] = {}
             end
@@ -54,44 +87,46 @@ local function get_dir_tree()
     return master
 end
 
-local function get_format(bars_to_ignore, indent)
-	local ret_string = ''
-	for i=1,indent do
-		if not bars_to_ignore[i] then
-			ret_string = ret_string.."┃   "
+local function get_line_tree_markers(markers_to_ignore, indent_level)
+	local line_tree_markers = ''
+	for i=1,indent_level do
+		if not markers_to_ignore[i] then
+			line_tree_markers = line_tree_markers..VERTICAL_BAR..DEFAULT_INDENT
 		else
-			ret_string = ret_string.."    "
+			line_tree_markers = line_tree_markers..' '..DEFAULT_INDENT
 		end
 	end
-	return ret_string
+	return line_tree_markers
 end
 
-local function dump(t, indent, bars_to_ignore)
+local function print_tree(t, indent_level, markers_to_ignore)
     local t = t or get_dir_tree()
-    local indent = indent or 0
-    local bars_to_ignore = bars_to_ignore or {}
+    local indent_level = indent_level or 0
+    local markers_to_ignore = markers_to_ignore or {}
     local names = {}
 
     for n,g in pairs(t) do
         table.insert(names,n)
     end
+
     table.sort(names)
     for i,n in pairs(names) do
         local v = t[n]
         local line_prefix = ''
-        if indent > 0 then
-        	local last_item_prefix = get_format(bars_to_ignore, indent - 1).."┣"
+        if indent_level > 0 then
+        	local last_item_prefix = get_line_tree_markers(markers_to_ignore, indent_level - 1)..MIDDLE_CONNECTOR
         	if i == #names then 
-        		last_item_prefix = get_format(bars_to_ignore, indent - 1).."┗"
-        		bars_to_ignore[indent] = true
+        		last_item_prefix = get_line_tree_markers(markers_to_ignore, indent_level - 1)..ENDING_CONNECTOR
+        		markers_to_ignore[indent_level] = true
         	end
-        	line_prefix = last_item_prefix.."━"
+        	line_prefix = last_item_prefix..HORIZONTAL_CONNECTOR
         end
         print(line_prefix..tostring(n))
-        dump(v, indent + 1, bars_to_ignore)
+        print_tree(v, indent_level + 1, markers_to_ignore)
         
     end
-    bars_to_ignore[indent] = nil
+    markers_to_ignore[indent_level] = nil
 end
 
-dump()
+print_tree()
+print(string.format("\n%s directories, %s files", num_dirs, num_files))
